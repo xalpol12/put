6fdfd62a27aa37bc294080e807b22c62eb07ffb0
xalpol12 <xalpol12@gmail.com>
2024-05-08 14:20:39 +0200

    [ws] create ws server init

---
+++ it-for-distributed-processing/websocket-project/backend/ws-server/src/test/java/com/xalpol12/wsserver/WsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xalpol12.wsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
