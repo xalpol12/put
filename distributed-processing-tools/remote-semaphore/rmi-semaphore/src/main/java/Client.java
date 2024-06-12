3024c3b44583d60a911477e6ab2c33eb7f8f0b61
xalpol12 <xalpol12@gmail.com>
2024-06-12 16:59:39 +0200

    add things for darek

--- distributed-processing-tools/remote-semaphore/rmi-semaphore/src/main/java/Client.java
+++ distributed-processing-tools/remote-semaphore/rmi-semaphore/src/main/java/Client.java
@@ -4,14 +4,10 @@ import java.rmi.registry.Registry;
 public class Client {
     public static void main(String[] args) {
         try {
-            if (args.length != 2) {
-                throw new Exception("Wrong argument count, usage: java Client <server_address> <number_of_permits>");
-            }
 
             Registry registry = LocateRegistry.getRegistry(args[0], Server.PORT);
             CustomSemaphore semaphore = (CustomSemaphore) registry.lookup(Server.NAME);
 
-            int permitsToAcquire = Integer.parseInt(args[1]);
             semaphore.acquire(permitsToAcquire);
             Thread.sleep(4000);
             semaphore.release(permitsToAcquire);
@@ -19,5 +15,31 @@ public class Client {
             System.err.println("Client exception: " + e);
             e.printStackTrace();
         }
+    }
+
+    public static void handleCommand(String[] args) throws Exception {
+        if (args.length != 2 || args.length != 3) {
+            throw new Exception("Wrong argument count, usage: java Client <server_address> <'acquire' or 'release'> <(optional) permits>");
+        } else if (args.length == 2) {
+            switch (args[1]) {
+                case "acquire":
+                    semaphore.acquire();
+                case "release":
+                    semaphore.release();
+                default:
+                    throw new Exception("Illegal operation. Options: 'acquire' or 'release'");
+            }
+        } else {
+            int permitsToAcquire = Integer.parseInt(args[2]);
+            switch (args[1]) {
+                case "acquire":
+                    semaphore.acquire();
+                case "release":
+                    semaphore.release();
+                default:
+                    throw new Exception("Illegal operation. Options: 'acquire' or 'release'");
+            }
+        }
+
     }
 }
